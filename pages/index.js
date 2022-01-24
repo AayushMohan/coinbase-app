966fc667ee03e2604ce6daf02988b95e52771b8a
AayushMohan <aayushmohan1702@gmail.com>
2022-01-24 18:05:47 +0530

    Added Styles in wallet conncet btn

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,28 @@
-import styled from "styled-components";
 import { useWeb3 } from "@3rdweb/hooks";
+import styled from "@emotion/styled";
 
 export default function Home() {
+  const { address, connectWallet } = useWeb3();
+
   return (
-    <div>
-      <h1>Hello world!</h1>
-    </div>
+    <Wrapper>
+      <WalletConnect>
+        <button onClick={() => connectWallet("injected")}>
+          Connect Wallet
+        </button>
+      </WalletConnect>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  height: 100vh;
+  max-width: 100vw;
+  background-color: #0a0b0d;
+  color: #fff;
+  display: grid;
+  place-items: center;
+`;
+
+const WalletConnect = styled.div``;
