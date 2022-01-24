9d3a631a63aad065ce7fa1e9c487abaad5a23494
AayushMohan <aayushmohan1702@gmail.com>
2022-01-24 17:43:49 +0530

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ThirdwebWeb3Provider } from "@3rdweb/hooks";
+
+const supportedChainsIds = [4, 80001];
+const connectors = {
+  injected: {},
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
