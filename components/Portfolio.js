743f75f8fe8a19f987303dc19fa054d29655f225
AayushMohan <aayushmohan1702@gmail.com>
2022-01-26 20:34:50 +0530

    commit

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -3,6 +3,7 @@ import React from "react";
 import { BsThreeDotsVertical } from "react-icons/bs";
 import { coins } from "../static/coins";
 import Coin from "./Coin";
+import BalanceChart from "./BalanceChart";
 
 const Portfolio = () => {
   return (
